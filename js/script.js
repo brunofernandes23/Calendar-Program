aac32ba207109a79140c363fb1beed978dcde664
Bruno Fernandes <80977237+brunofernandes23@users.noreply.github.com>
2021-11-17 15:51:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-function exibir(){
-    var mes = parseInt(document.getElementById("numero").value);
+function display(){
+    let month = parseInt(document.getElementById("number").value);
     
-   switch(mes){
-     case 1:  alert('Janeiro'); break;
-     case 2:  alert('Fevereiro'); break;
-     case 3:  alert('Março'); break;
-     case 4:  alert('Abril'); break;
-     case 5:  alert('Maio'); break;
-     case 6:  alert('Junho'); break;
-     case 7:  alert('Julho'); break;
-     case 8:  alert('Agosto'); break;
-     case 9:  alert('Setembro'); break;
-     case 10: alert('Outubro'); break;
-     case 11: alert('Novembro'); break;
-     case 12: alert('Dezembro'); break;
-     default: alert('Mês Inválido!'); break;
+   switch(month){
+     case 1:  alert('January'); break;
+     case 2:  alert('February'); break;
+     case 3:  alert('March'); break;
+     case 4:  alert('April'); break;
+     case 5:  alert('May'); break;
+     case 6:  alert('June'); break;
+     case 7:  alert('July'); break;
+     case 8:  alert('August'); break;
+     case 9:  alert('September'); break;
+     case 10: alert('October'); break;
+     case 11: alert('November'); break;
+     case 12: alert('December'); break;
+     default: alert('Invalid month!'); break;
     }
    }
